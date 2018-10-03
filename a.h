746e0f4b8c0ae7b2684594b75049597fe58a5540
anil uluturk <anilu@comodo.net>
2018-10-03 17:11:03 +0300

    added doxygen support and doxyfile

---
+++ a.h
@@ -0,0 +1,19 @@
+/** Short comment: This is for .... .
+
+    After an empty line, here comes the main description of the code
+    entity. Here can be a longer text, examples,
+    philosophical considerations and special solution descriptions.
+    After this text, only @-tags are allowed. Each following text 
+belongs
+    to its prefacing @-tag.
+
+    @param integer1 [0 .. 255]
+    Describe parameter integer1.
+
+    @param str2
+    Describe the parameter str2.
+
+    @return true, if everything is ok. Else false.
+*/
+
+bool   funct_00( int integer1, const char * str2 );
